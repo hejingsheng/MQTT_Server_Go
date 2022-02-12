2c80264f22c39985e3e53650052137804186817c
hejingsheng <hejingsheng@qq.com>
2022-02-12 09:49:17 +0800

    base TCP Server

--- mqtt_server.go
+++ mqtt_server.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
 	fmt.Println("MQTT Server Running...")
+
+	listen, err := net.Listen("tcp", "0.0.0.0:1883");
+	if err != nil {
+		fmt.Println("listen error");
+		return;
+	}
+	defer listen.Close()
+
+	for {
+		fmt.Println("wait a client connect")
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("client connect error")
+		} else {
+			fmt.Printf("a client connect success %v\n", conn.RemoteAddr().String())
+		}
+
+	}
 }
