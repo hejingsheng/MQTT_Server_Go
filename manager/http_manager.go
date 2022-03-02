239bda75850c5dea8513b89bfa17ef4dacd60e1c
hjs <he@tietong-wl.com>
2022-03-02 17:59:02 +0800

    add offlinemsg http interface

--- manager/http_manager.go
+++ manager/http_manager.go
@@ -4,17 +4,18 @@ import (
 	"net/http"
 )
 
-
 var mqttServerMux http.ServeMux
 var onLineNumHandle OnLineNumHandle
 var clientListHandle ClientInfoListHandle
 var clientSubHandle ClientSubInfoHandle
+var clientOffMsgHandle ClientOfflineHandle
 
 func Http_Manager_Server() {
 
 	mqttServerMux.Handle("/getOnLineNum", &onLineNumHandle)
 	mqttServerMux.Handle("/getClientList", &clientListHandle)
 	mqttServerMux.Handle("/getClientSub", &clientSubHandle)
+	mqttServerMux.Handle("/getOfflienMsg", &clientOffMsgHandle)
 
 	server := &http.Server{Addr: "127.0.0.1:9000", Handler: &mqttServerMux}
 	server.ListenAndServe()
